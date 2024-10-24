52d0f92d5053f2aa3ccc05f32c2bb17b6168a7e9
Nima HeydariNasab <nima.heydari79@yahoo.com>
2024-10-24 12:40:41 +0330

    feat: add result cache

--- src/Analyser.php
+++ src/Analyser.php
@@ -19,14 +19,41 @@ final class Analyser
      */
     public static function analyse(array $files, Closure $callback): void
     {
+        $cacheFilepath = '.pest_type_coverage_cache';
+
+        $cache = is_file($cacheFilepath)
+            ? unserialize(file_get_contents($cacheFilepath))
+            : [];
+
         $testCase = new TestCaseForTypeCoverage;
 
+        $cacheIsModified = false;
         foreach ($files as $file) {
-            $errors = $testCase->gatherAnalyserErrors([$file]);
-            $ignored = $testCase->getIgnoredErrors();
+            $fileHash = md5_file($file);
+            if (array_key_exists($file, $cache) && $cache[$file]['hash'] === $fileHash) {
+                $errors = $cache[$file]['errors'];
+                $ignored = $cache[$file]['ignored'];
+            } else {
+                $errors = $testCase->gatherAnalyserErrors([$file]);
+                $ignored = $testCase->getIgnoredErrors();
+                $cache[$file] = [
+                    'hash' => $fileHash,
+                    'errors' => $errors,
+                    'ignored' => $ignored,
+                ];
+                $cacheIsModified = true;
+            }
             $testCase->resetIgnoredErrors();
 
             $callback(Result::fromPHPStanErrors($file, $errors, $ignored));
+        }
+        foreach (array_keys($cache) as $file) {
+            if (! is_file($file)) {
+                unset($cache[$file]);
+            }
+        }
+        if ($cacheIsModified) {
+            file_put_contents($cacheFilepath, serialize($cache));
         }
     }
 }
